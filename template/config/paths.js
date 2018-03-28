f736097e1b7d41a7a6acf9963c8d40c8735d7f09
韦森 <723955399@qq.com>
2018-03-28 16:52:45 +0800

    feat: upgrade to 0.1.2

--- template/config/paths.js
+++ template/config/paths.js
@@ -8,6 +8,7 @@ const url = require('url');
 // https://github.com/facebookincubator/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const resolveOwn = relativePath => path.resolve(__dirname, '..', relativePath);
 
 const envPublicUrl = process.env.PUBLIC_URL;
 
@@ -40,6 +41,7 @@ function getServedPath(appPackageJson) {
 
 // config after eject: we're in ./config/
 module.exports = {
+  ownPath: resolveOwn('.'),
   dotenv: resolveApp('.env'),
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
