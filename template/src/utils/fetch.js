6589f2c413a6d98f339884fc97f6c81259276d35
韦森 <723955399@qq.com>
2018-04-17 13:09:11 +0800

    feat: update fetch

--- template/src/utils/fetch.js
+++ template/src/utils/fetch.js
@@ -4,29 +4,28 @@ import { baseUrl } from './config'
 import { globalShowSpinAction, globalShowSpinTipAction } from '../global/globalReduck'
 import { notification } from 'antd'
 
-const userInfo = storage.get('user')
-
 let fetcher = axios.create({
   method: 'post',
   baseURL: baseUrl,
   withCredentials: true,
   transformRequest: [
     function(data) {
-      if (userInfo && data && !data.NOUSERINFO) {
-        data.accessToken = userInfo.accessToken
-      }
       return JSON.stringify(data)
     }
   ],
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
-    accessToken: userInfo && userInfo.accessToken
   }
 })
 
+// 网络请求request拦截器
 fetcher.interceptors.request.use(
   function(config) {
+    if (config && config.data) {
+      // TODO 对请求参数进行包装
+      config.data.accessToken = storage.get('accessToken')
+    }
     return config
   },
   function(error) {
@@ -34,52 +33,48 @@ fetcher.interceptors.request.use(
   }
 )
 
-fetcher.interceptors.response.use(
-  function(response) {
-    if (response.data.code === 89001 || response.data.code === 81001) {
-      location.href = '/login'
-    }
+// 网络请求response拦截器
+fetcher.interceptors.response.use(function (response) {
+  if (response.data.code === 60001 || response.data.code === 60002 || response.data.code === 60003 ||
+    response.data.code === 60004 || response.data.code === 60005) {
+    // TODO 对特定异常特殊处理
+    notification['error']({
+      message: '警告',
+      description: response.data && response.data.errmsg
+    })
+  } else {
     return response.data
-  },
-  function(error) {
-    return Promise.reject(error)
   }
-)
+}, function (err) {
+  if (err.toString().startsWith('Network Error') >= 0) {
+    notification['warning']({
+      message: '警告',
+      description: '网络异常，请检查当前互联网状态'
+    })
+  } else if (err.toString().startsWith('Request failed with status code')) {
+    notification['warning']({
+      message: '警告',
+      description: '接口异常'
+    })
+  } else {
+    notification['warning']({
+      message: '警告',
+      description: err.toString()
+    })
+  }
+})
 
 export default function fetch(dispatch, apiUrl, arg, showLoading = true, content = '') {
-  return new Promise((resolve, reject) => {
-    // showLoading && dispatch(showSpin({ bool: true, content }))
-    showLoading && dispatch(globalShowSpinAction(true))
-    content && dispatch(globalShowSpinTipAction(content))
-    fetcher
-      .post(apiUrl, arg)
-      .then(data => {
-        showLoading && dispatch(globalShowSpinAction(false))
-        content && dispatch(globalShowSpinTipAction(''))
-        // showLoading && dispatch(showSpin({ bool: false, content: '' }))
-        resolve(data)
-      })
-      .catch(err => {
-        // dispatch(showSpin({ bool: false, content: '' }))
-        showLoading && dispatch(globalShowSpinAction(false))
-        content && dispatch(globalShowSpinTipAction(''))
-        if (err.toString().indexOf('Network Error') >= 0) {
-          notification['warning']({
-            message: '警告',
-            description: '网络异常，请检查当前互联网状态'
-          })
-        } else if (err.toString().indexOf('Request failed with status code 404')) {
-          notification['warning']({
-            message: '警告',
-            description: '接口异常'
-          })
-        } else {
-          notification['warning']({
-            message: '警告',
-            description: err.toString()
-          })
-        }
-        reject(err)
-      })
-  })
+  showLoading && dispatch(globalShowSpinAction(true))
+  content && dispatch(globalShowSpinTipAction(content))
+  return fetcher.post(apiUrl, arg)
+    .then(data => {
+      showLoading && dispatch(globalShowSpinAction(false))
+      content && dispatch(globalShowSpinTipAction(''))
+      if (data) {
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
 }
